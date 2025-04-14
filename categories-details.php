ce893cd6d5797840dc8965d87c974a8a63de50ba
VIkcey <163313547+VIkcey@users.noreply.github.com>
2025-04-14 14:46:46 -0400

    add new files

--- categories-details.php
+++ categories-details.php
@@ -7,14 +7,11 @@ session_start(); // Start the session
 
 $id = $_GET['id'];
 if (!$id) {
-    include_once 'page-not-found.php';
+    include 'page-not-found.php';
 }
 
-// print_r($id);
-// exit;
-
 $sql = "SELECT
-      A.title,   A.summary,     A.content,
+      A.id AS article_id, A.title,   A.summary,     A.content,
       A.created, A.category_id, A.member_id, 
       C.name AS category,
       CONCAT( M.forename, ' ', M.surname ) AS author,
@@ -25,21 +22,25 @@ $sql = "SELECT
     JOIN member      AS M  ON A.member_id   = M.id
     LEFT JOIN image  AS I  ON A.image_id    = I.id
     WHERE
-      A.id = :id AND
+      A.category_id        = :id AND
       A.published = 1
     ;"
 ;
 
-$article = pdo($pdo, $sql, ['id' => $id])->fetch();
+$article = pdo($pdo, $sql, ['id' => $id])->fetchAll();
 
 if (!$article) {
     include 'page-not-found.php';
 }
-// print_r($popular_cat);
+
+
+// print_r($article);
 // exit;
 
 ?>
+
 <?php include_once 'includes/header.php'; ?>
+
 <div class="container">
     <section class="hero">
         <div class="hero-content">
@@ -51,49 +52,19 @@ if (!$article) {
     <section class="">
         <h2 class="category-title">Articles in Asia</h2>
         <div class="post-grid nav-container">
-            <a href="articles-details" class="post-card">
-                <img src="img/bali.jpg" alt="Hidden Beaches of Bali" class="post-image">
-                <div class="post-info">
-                    <h3>Hidden Beaches of Bali</h3>
-                    <p>April 4, 2025</p>
-                </div>
-            </a>
-            <a href="articles-details" class="post-card">
-                <img src="img/places-to-see-cherry-blossoms-hirano-shrine.jpg" alt="Cherry Blossoms in Kyoto"
-                    class="post-image">
-                <div class="post-info">
-                    <h3>Cherry Blossoms in Kyoto</h3>
-                    <p>March 15, 2025</p>
-                </div>
-            </a>
-            <a href="articles-details" class="post-card">
-                <img src="img/Thai-street-food.jpg" alt="Street Food in Bangkok" class="post-image">
-                <div class="post-info">
-                    <h3>Street Food in Bangkok</h3>
-                    <p>February 28, 2025</p>
-                </div>
-            </a>
-            <a href="articles-details" class="post-card">
-                <img src="img/doris-overview-2048x1349.jpg" alt="Cruising Halong Bay" class="post-image">
-                <div class="post-info">
-                    <h3>Cruising Halong Bay</h3>
-                    <p>February 10, 2025</p>
-                </div>
-            </a>
-            <a href="articles-details" class="post-card">
-                <img src="img/Top-10-Trekking-Places-of-Himalayas.jpg" alt="Trekking the Himalayas" class="post-image">
-                <div class="post-info">
-                    <h3>Trekking the Himalayas</h3>
-                    <p>January 25, 2025</p>
-                </div>
-            </a>
-            <a href="articles-details" class="post-card">
-                <img src="img/asia-holi.jpeg" alt="Colors of Holi in India" class="post-image">
-                <div class="post-info">
-                    <h3>Colors of Holi in India</h3>
-                    <p>January 5, 2025</p>
-                </div>
-            </a>
+            <?php foreach ($article as $art) { ?>
+                <a href="articles-details?id=<?= $art['article_id'] ?>" class="post-card">
+                    <img src="<?= 'articles/' . $art['image_file'] ?>" alt="<?= $art['title'] ?>" class="post-image">
+                    <div class="post-info">
+                        <h3><?= $art['title'] ?></h3>
+                        <p><?= date("F j, Y", strtotime($art['created'])) ?></p>
+                        <div>
+                            <?= "Posted in" . " " . $art['category'] . "by " ?>
+                            <?= $art['author'] ?>
+                        </div>
+                    </div>
+                </a>
+            <?php } ?>
         </div>
     </section>
 </div>
